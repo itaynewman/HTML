f9dc6751162e945b38824a95b9f77db6c2b4b365
itaynewman <itaynewman@gmail.com>
2023-05-11 18:27:55 +0300

    changes

---
+++ index.js
@@ -0,0 +1,20 @@
+function addition(a, b){
+    var c = a + b
+    return c
+}
+function minus(a, b){
+    var d = a - b
+    return d
+}
+function multiply(a, b){
+    var e = a * b
+    return e
+}
+function divide(a, b){
+    var f = a / b
+    return f
+}
+addition(10, 10)
+minus(10, 10)
+multiply(10, 10)
+divide(10, 10)
